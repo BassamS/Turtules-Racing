9b2c7d90aa9fbc0aa0834cda2af2de3a6ed38607
Bassam AL SAEDY <basaidi@gmail.com>
2022-10-06 06:55:51 +0200

    Turtle move

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
 import turtle
+import time
 
 WIDTH, HEIGHT = 500, 500
-
-screen = turtle.Screen()
-screen.setup(WIDTH, HEIGHT)
 
 
 def get_number_of_racers():
@@ -22,5 +20,22 @@ def get_number_of_racers():
             print('Number not in range 2-10. Try Again!')
 
 
+def init_turtle():
+    screen = turtle.Screen()
+    screen.setup(WIDTH, HEIGHT)
+    # Screen Title
+    screen.title('Turtle Racing!')
+
+
 racers = get_number_of_racers()
-print(racers)
+init_turtle()
+
+racer = turtle.Turtle()
+racer.speed(1)
+racer.shape('turtle')
+racer.color('cyan')
+racer.forward(100)
+racer.left(90)
+racer.forward(100)
+racer.right(90)
+racer.backward(100)
